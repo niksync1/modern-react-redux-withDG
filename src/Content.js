26f2c6062948c821a0029b85adbb69f8baaa41ef
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-04-17 00:20:52 +0000

    added CRUD operations with fetchand JSON server

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { FaTrashAlt } from 'react-icons/fa';
 import ItemList from './ItemList';
 
 const Content = ({items, handleCheck, handleDelete}) => {
 
   return (
-    <main >
+    <>
         {items.length ? (
            <ItemList 
            items= {items}
@@ -15,7 +14,7 @@ const Content = ({items, handleCheck, handleDelete}) => {
             ) : (
                 <p style={{ marginTop: '2rem' }}>Your list is empty.</p>   
         )}
-    </main>
+    </>
   )
 }
 
